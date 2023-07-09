b09eb97e9ddb593cdf89536ee3cb596d2da724b4
SevvyYilmaz <yilmaz_S@outlook.de>
2023-07-09 16:19:49 -0700

    Fetch & Catch Pokemon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import {useEffect, useState} from 'react';
 import './App.css';
 
+
+
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
+      .then(response => {
+        return response.json();
+      }).then(response => {
+        console.log(response);
+        setPokemon(response.results);
+      }).catch(err=>{
+        console.log(err);
+      });
+      
+  }, []);
+  const capitalizeFirstLetter = (name) => {
+    return name.charAt(0).toUpperCase() + name.substring(1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h2>Fetch Pokemon</h2>
+      {pokemon.map((pokemon, index)=>(
+        <p key = {index}>{capitalizeFirstLetter(pokemon.name)}</p>
+      ))}
     </div>
   );
 }
